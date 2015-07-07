246f46c3e3ab0bd5ab0e13c8042912781685c6b3
Ryan Moran and Zachary Gershman <rmoran@pivotal.io>
2015-07-07 13:53:04 -0700

    Adds some more docs for the internal/documents package

--- internal/documents/clients.go
+++ internal/documents/clients.go
@@ -1,20 +1,57 @@
 package documents
 
-type ClientResponse struct {
-	ClientID             string   `json:"client_id"`
-	Scope                []string `json:"scope"`
-	ResourceIDs          []string `json:"resource_ids"`
-	Authorities          []string `json:"authorities"`
+// CreateClientRequest represents the JSON transport data structure
+// for a request to create a Client.
+type CreateClientRequest struct {
+	// ClientID is the unique identifier specifying the client.
+	ClientID string `json:"client_id"`
+
+	// ClientSecret is the secret value used to fetch a token
+	// for the client.
+	ClientSecret string `json:"client_secret"`
+
+	// Scope is a list of permission values to apply to user tokens that
+	// are granted to the client.
+	Scope []string `json:"scope"`
+
+	// ResourceIDs is a list of audiences for the client. This field
+	// is always ["none"].
+	ResourceIDs []string `json:"resource_ids"`
+
+	// Authorities is a list of permission values applied when the client
+	// fetches their own token.
+	Authorities []string `json:"authorities"`
+
+	// AuthorizedGrantTypes is a list of grant types applied to the client.
 	AuthorizedGrantTypes []string `json:"authorized_grant_types"`
-	AccessTokenValidity  int      `json:"access_token_validity"`
+
+	// AccessTokenValidity is the number of seconds before a token granted
+	// to this client will expire.
+	AccessTokenValidity int `json:"access_token_validity"`
 }
 
-type CreateClientRequest struct {
-	ClientID             string   `json:"client_id"`
-	ClientSecret         string   `json:"client_secret"`
-	Scope                []string `json:"scope"`
-	ResourceIDs          []string `json:"resource_ids"`
-	Authorities          []string `json:"authorities"`
+// ClientResponse represents the JSON transport data structure for
+// a response containing a Client resource.
+type ClientResponse struct {
+	// ClientID is the unique identifier specifying the client.
+	ClientID string `json:"client_id"`
+
+	// Scope is a list of permission values to apply to user tokens that
+	// are granted to the client.
+	Scope []string `json:"scope"`
+
+	// ResourceIDs is a list of audiences for the client. This field
+	// is always ["none"].
+	ResourceIDs []string `json:"resource_ids"`
+
+	// Authorities is a list of permission values applied when the client
+	// fetches their own token.
+	Authorities []string `json:"authorities"`
+
+	// AuthorizedGrantTypes is a list of grant types applied to the client.
 	AuthorizedGrantTypes []string `json:"authorized_grant_types"`
-	AccessTokenValidity  int      `json:"access_token_validity"`
+
+	// AccessTokenValidity is the number of seconds before a token granted
+	// to this client will expire.
+	AccessTokenValidity int `json:"access_token_validity"`
 }
